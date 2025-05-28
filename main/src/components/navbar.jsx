473d45c28f8a99ed812df761ab5d684a25da61d6
Apikshya Shrestha <apikshyashrestha@gmail.com>
2025-05-28 20:02:12 +0545

    installing all req

---
+++ main/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import { link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <img src="./images/logo.png"></img>
+        </div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
--- main/tailwind.config.js
+++ main/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purple:{
+          100: "#C4A8CB",
+          200: "#A368C0",
+          300: "#7D3C98",
+        },
+        orange: {
+          100: "#FFD6C2",
+          200: "#FFD6C2",
+          300: "#FFD6C2",
+          400: "#E07A5F",
+        },
+        green: {
+          100: "#A3B18A",
+        },
+        yellow:{
+          100:"#F7F3EF",
+        },
+      }
+    },
   },
   plugins: [],
 }
