68b13d8685560225660bd267da5076bae46f2bc8
Apikshya Shrestha <apikshyashrestha@gmail.com>
2025-05-28 20:35:02 +0545

    nthg

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-white-100">
-        <div className="">
-          
-        </div>
-      </div>
+    <div>hello</div>
+  
+      
     </>
   );
 }
